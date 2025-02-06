84c33f0b046d2f3fdbef6576bee2a5d7c81c3f48
Chadin Chaipornpisuth <ping47024@icloud.com>
2025-02-06 20:22:28 +0700

    hide by user @

--- content.js
+++ content.js
@@ -1,17 +1,28 @@
 (function () {
   "use strict";
-  const regex =
+  const userRegex = /\@.+\-.{3}/
+  const commentRegex =
     /[^a-zA-Z0-9\.\d\n\u0E00-\u0E7F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF\u3000-\u303F\s\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~\,\'\"\!\…]|max33|แตกดี|กำไรเยอะ|สนุกจริง\s?ๆ/i;
   setInterval(() => {
     const comments = document.querySelectorAll("#content-text");
-
+    
     comments.forEach((comment) => {
-      if (regex.test(comment.innerText)) {
+      const component = comment.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+      const userName =
+        comment.parentElement.parentElement.parentElement.querySelector(
+          "span"
+        ).innerText;
+      if (
+        userRegex.test(userName)
+        // || 
+        // commentRegex.test(comment.innerText) 
+      ) {
         // Hide the entire comment container
-        const component = comment.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
         // component.style.display = "none";
-        component.innerHTML = "<button disabled>Comment Hidden</button>";
-        console.log(`comments hidden`);
+        console.log(
+          `User Comment Hidden : ${userName} => ${comment.innerText}`
+        );
+        component.innerHTML = "<button disabled>User Comment Hidden</button>";
       }
     });
   }, 1000);
